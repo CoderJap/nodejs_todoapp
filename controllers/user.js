2a22f53d6a39f2bbca48c3180dc4045659fa7b39
Japjot Singh Nanda <japjotwork007@gmail.com>
2024-08-04 15:43:52 +0530

    fixed logout controller

--- controllers/user.js
+++ controllers/user.js
@@ -56,7 +56,7 @@ export const getMyProfile = (req, res) => {
   }
 }
 
-export const logout = () => {
+export const logout = (req,res) => {
 
   res.status(200).cookie("token","",{
     expires:new Date(Date.now()),
